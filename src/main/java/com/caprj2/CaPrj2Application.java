d3d7e30db505451ca582743c5dd61032c8ff4346
chaiheung <chaiheung@naver.com>
2024-05-07 14:36:33 +0900

    프로젝트 생성

---
+++ src/main/java/com/caprj2/CaPrj2Application.java
@@ -0,0 +1,13 @@
+package com.caprj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaPrj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaPrj2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/caprj2/CaPrj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caprj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaPrj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
